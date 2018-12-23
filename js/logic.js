c63a499a3aab4bd0c148ba0d9ab9eea56a444290
martinvicknair <32040730+martinvicknair@users.noreply.github.com>
2018-12-23 14:40:06 -0600

    first two sections

--- js/logic.js
+++ js/logic.js
@@ -5,6 +5,8 @@ var siteSupervisor = "";
 var mealType = "";
 var mealPlural = "";
 
+var totalMealsStart = "";
+
 var dt = new Date();
 var dateOptions = {
   weekday: 'short',
@@ -20,11 +22,30 @@ document.getElementById("siteName-input").value = localStorage.getItem("mealCoun
 document.getElementById("siteAddress-input").value = localStorage.getItem("mealCounter-siteAddress");
 document.getElementById("siteSupervisor-input").value = localStorage.getItem("mealCounter-siteSupervisor");
 
+// https://getbootstrap.com/docs/4.2/components/forms/#validation
+// https://www.codeply.com/go/LYdmkkTZUS/bootstrap-4-validation-example
+$("#meal-type-btns").click(function(event) {
+  // Fetch form to apply custom Bootstrap validation
+  var form = $("#site-meal-form")
+  if (form[0].checkValidity() === false) {
+    event.preventDefault()
+    event.stopPropagation()
+  }
+  else if (form[0].checkValidity() == true) {
+      event.preventDefault()
+      var meal = document.activeElement.getAttribute('value');
+      selectMeal(meal)
+      $("#site-meal-card").toggleClass('d-none');
+      $("#meals-available-card").toggleClass('d-none')
+}
+  form.addClass('was-validated');
+});
+
 function selectMeal(meal) {
   siteName = document.getElementById("siteName-input").value;
   siteAddress = document.getElementById("siteAddress-input").value;
   siteSupervisor = document.getElementById("siteSupervisor-input").value;
-  console.log(siteName);
+
   localStorage.setItem("mealCounter-siteName", siteName);
   localStorage.setItem("mealCounter-siteAddress", siteAddress);
   localStorage.setItem("mealCounter-siteSupervisor", siteSupervisor);
@@ -42,58 +63,55 @@ function selectMeal(meal) {
     mealType = "Supper";
     mealPlural = "Suppers";
   }
-  console.log(mealType);
+  console.log(`${siteName}-${shortDate}-${mealType}`);
 }
-// https://www.codeply.com/go/LYdmkkTZUS/bootstrap-4-validation-example
-$("#meal-type").click(function(event) {
-  // Fetch form to apply custom Bootstrap validation
-  var form = $("#site-meal-form")
-  var meal = document.activeElement.getAttribute('value');
-  if (form[0].checkValidity() === false) {
-    event.preventDefault()
-    event.stopPropagation()
+
+function sumMeals() {
+  var mealsReceived = document.getElementById("meals-received").value;
+  var mealsLeftover = document.getElementById("meals-leftover").value;
+  if (mealsReceived == "") {mealsReceived=0};
+  if (mealsLeftover == "") {mealsLeftover=0};
+  totalMealsStart = parseInt(mealsReceived) + parseInt(mealsLeftover);
+  var totalMealsText = totalMealsStart.toString();
+  document.getElementById("total-meals-start").value = totalMealsStart;
+  console.log(`totalMealsStart: ${totalMealsStart}`);
+  if (totalMealsStart >= 1) {
+    document.getElementById("startcounting-btn").setAttribute("class", "btn-lg w-100 btn-success");
   }
-  else if (form[0].checkValidity() == true) {
-      event.preventDefault()
-      selectMeal(meal)
-      $("#site-meal-info").toggleClass('d-none');
-      $("#meals-available").toggleClass('d-none')
+  else {
+    document.getElementById("startcounting-btn").setAttribute("class", "btn-lg w-100 btn-danger");
+    // document.getElementById("meals-available-form").setAttribute("class", "is-invalid");
+  }
 }
-  form.addClass('was-validated');
-});
+
+// function updateTotal() {
+//     var left = document.getElementById("left").value;
+//     var mDel = document.getElementById("mDel").value;
+// 	if (left=="") {left=0;}
+// 	if (mDel=="") { mDel=0; }
+//     mTotal = parseInt(left) + parseInt(mDel);
+//     var mTotalText = mTotal.toString();
+//     document.getElementById("mTotl").value = mTotalText ;
+//     var button = document.getElementById("button8");
+//     button.style.display='inline-block';
+// }
 
 $("#startcounting-btn").click(function(event) {
   // Fetch form to apply custom Bootstrap validation
   var form = $("#meals-available-form")
-  
   if (form[0].checkValidity() === false) {
     event.preventDefault()
     event.stopPropagation()
   }
+  if (totalMealsStart == 0) {
+    event.preventDefault()
+    event.stopPropagation()
+    form.removeClass('was-validated')
+  }
   else if (form[0].checkValidity() == true) {
       event.preventDefault()
-
-      $("#meals-available").toggleClass('d-none')
-      $("#meal-counter").toggleClass('d-none');
+      $("#meals-available-card").toggleClass('d-none')
+      $("#meal-count-card").toggleClass('d-none');
 }
   form.addClass('was-validated');
 });
-
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-// (function() {
-//   'use strict';
-//   window.addEventListener('load', function() {
-//     // Fetch all the forms we want to apply custom Bootstrap validation styles to
-//     var forms = document.getElementsByClassName('needs-validation');
-//     // Loop over them and prevent submission
-//     var validation = Array.prototype.filter.call(forms, function(form) {
-//       form.addEventListener('submit', function(event) {
-//         if (form.checkValidity() === false) {
-//           event.preventDefault();
-//           event.stopPropagation();
-//         }
-//         form.classList.add('was-validated');
-//       }, false);
-//     });
-//   }, false);
-// })();
